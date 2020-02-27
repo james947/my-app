050ef470892eac30941091f98db2dcd7cd75b444
james947 <jmuriuki947@gmail.com>
2020-02-28 01:27:21 +0300

    Add icons

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600] ,
       ),
       body: Center(
-        child: Image.asset('assets/space.jpg')
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50.0,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed:() {} ,
